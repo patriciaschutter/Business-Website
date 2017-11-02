73d47d2adc367cfd80b9b27d0104550519acc51c
Patricia Schutter <patricia_schutter@hotmail.com>
2017-11-02 14:55:14 +0100

    update gallery

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -18,12 +18,37 @@ function showDivs(n) {
 }
 
 // GALERY 2
-function showDivs(n) {
-  var x = document.getElementsByClassName("galery1");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
+var slideIndex_02 = 1;
+showDivs_02(slideIndex_02);
+
+function plusDivs_02(n) {
+  showDivs_02(slideIndex_02 += n);
+}
+
+function showDivs_02(n) {
+  var x = document.getElementsByClassName("galery2");
+  if (n > x.length) {slideIndex_02 = 1}    
+  if (n < 1) {slideIndex_02 = x.length}
   for (var i = 0; i < x.length; i++) {
      x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex_02-1].style.display = "block";  
+}
+
+// GALERY 3
+var slideIndex_03 = 1;
+showDivs_03(slideIndex_03);
+
+function plusDivs_03(n) {
+  showDivs_03(slideIndex_03 += n);
+}
+
+function showDivs_03(n) {
+  var x = document.getElementsByClassName("galery3");
+  if (n > x.length) {slideIndex_03 = 1}    
+  if (n < 1) {slideIndex_03 = x.length}
+  for (var i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex_03-1].style.display = "block";  
 }
