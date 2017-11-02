e925c07029eafab72d11029dc8f191fccbb047ee
Patricia Schutter <patricia_schutter@hotmail.com>
2017-11-02 16:36:42 +0100

    styling added

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,4 +1,4 @@
-
+	
 // HOME PAGE
 // SLIDESHOW HOME PAGE
 var slideIndex = 0;
@@ -12,5 +12,5 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 4000); // Change image every 2 seconds
+    setTimeout(carousel, 4000); // Change image every 4 seconds
 }
