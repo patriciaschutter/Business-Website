548c7eb08e2d1d520cefa421a44c2962c2c2375b
yoeriwalstra <yoeriwalstra@gmail.com>
2017-11-03 07:22:08 +0100

    centered divs in About and added enlarge nav buttons on hoover (not working perfect yet)

--- javascript/general.js
+++ javascript/general.js
@@ -1,1 +1,13 @@
 // GENERAL JS FOR ALL PAGES 
+
+/*ENLARGE NAV BUTTONS*/
+
+function bigImg(x) {
+    x.style.height = "50px";
+    x.style.width = "144px";
+}
+
+function normalImg(x) {
+    x.style.height = "auto";
+    x.style.width = "auto";
+}
