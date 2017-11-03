2fde130c9a3816bca948a96d2b6e1611605ab69e
yoeriwalstra <yoeriwalstra@gmail.com>
2017-11-03 11:11:32 +0100

    deleted nav bar resize

--- javascript/general.js
+++ javascript/general.js
@@ -1,13 +1,1 @@
 // GENERAL JS FOR ALL PAGES 
-
-/*ENLARGE NAV BUTTONS*/
-
-function bigImg(x) {
-    x.style.height = "50px";
-    x.style.width = "144px";
-}
-
-function normalImg(x) {
-    x.style.height = "auto";
-    x.style.width = "auto";
-}
